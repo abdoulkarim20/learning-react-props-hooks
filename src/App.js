ab3dd8f3f97f4544d29a6b5dedeb2baad3c9ea5e
abdoulkarim20 <abdoulkarimfoly1996@gmail.com>
2022-10-27 22:31:50 +0000

    fin de mise en pratique des hooks props

--- src/App.js
+++ src/App.js
@@ -5,14 +5,27 @@ import AddTechno from './pages/AddTechno';
 import AllTechos from './pages/AllTechos';
 import { Route, Routes } from 'react-router-dom';
 import Menu from './components/Menu';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useLocalStorage } from './hooks/useLocalStrage';
 function App() {
   const [technos, setTechno] = useState([])
+  /*Use effect est charger q'une seule fois
+  en un mot il nous permet de faire une surveillance*/
+  const STORAGE_KEY = 'technos';
+  const [tectnoStorage, setTechnoStorge] = useLocalStorage(STORAGE_KEY, []);
+  useEffect(() => {
+    console.log('useEffect with []');
+    setTechno(tectnoStorage)
+  }, [])
+  useEffect(() => {
+    console.log('useEffect with [techns]');
+    setTechnoStorge(technos);
+  }, [technos])
   /*un objet de techno
   [{name:"react",categorie:"front end",description:"objectif devenir un MERN"}]*/
   const handleAddTechno = (techno) => {
     setTechno([...technos, { ...techno, technoid: uuidv4() }])
-    console.log('la techno ajoute est:', techno);
+    // console.log('la techno ajoute est:', techno);
   }
   const handleDelete = (id) => {
     /*React Prop drilling*/
